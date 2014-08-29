9a0597a25810dd759b2b79f5e3bb619c8a1535da
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-08-29 04:24:19 +0100

    script

--- scripts/sente_cite.sh
+++ scripts/sente_cite.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+printf '\cite{'
 osascript <<APPLESCRIPT | 
 tell application "Sente 6"
     set refs to select references current library as tags
-	create bibliography elements current library from tags refs with "LaTeX" for bibliography
+	create bibliography elements current library from tags refs with "LaTeXKeys" for bibliography
 end tell
 APPLESCRIPT
-sed 's/<br>/\'$'\n/g' && osascript <<APPLESCRIPT 
+sed 's/<br>/\, /g' | ghead -c -1 && printf '}' && osascript <<APPLESCRIPT 
 tell application "System Events" to keystroke tab using command down
 APPLESCRIPT
